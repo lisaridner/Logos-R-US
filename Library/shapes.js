c41d31660ba5a9ae96e4a738a596ff0b984e7d0f
Lisa Ridner <125208782+lisaridner@users.noreply.github.com>
2023-05-01 19:56:20 -0400

    added parent classes and sub classes with shape points

--- Library/shapes.js
+++ Library/shapes.js
@@ -19,9 +19,24 @@ render(){
     return `<circle cx="25" cy="75" r="20" stroke="${this.color}" fill="${this.color}" stroke-width="5"/>`
 }
 }
-// square return syntax <rect x="10" y="10" width="30" height="30" stroke="black" fill="transparent" stroke-width="5"/>
-//  
-// triangle syntax <polygon points="200,10 250,210 160,210" 
-    //   stroke="green" fill="transparent" stroke-width="5"/>
+
+class Square extends Shape {
+    constructor (color){
+        super(color)
+    }
+render(){
+return `<rect x="10" y="10" width="30" height="30" stroke="black" fill="transparent" stroke-width="5"/>`
+}
+}
+
+class Triangle extends Shape {
+    constructor(color){
+        super(color)
+    }
+render(){
+return `<polygon points="200,10 250,210 160,210" 
+//   stroke="green" fill="transparent" stroke-width="5"/>1`
+}
+}
 
 module.exports={Shape,Circle, Triangle, Square}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer")
 const fs = require("fs") 
 
+
 // Ask your user for their criteria of their SVG logo
 // Add require for classes at the top of the file 
 // Create a function that uses inquirer to ask your users what they want - use this info to give to your class
